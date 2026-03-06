9f7e9c8ae431d93f0d93080dd989e6b5a788556c
siddik-tk <siddik3tk@gmail.com>
2026-03-06 18:21:16 +0530

    upgrade (day 23)

--- SOC/Bruteforce_Detector.py
+++ SOC/Bruteforce_Detector.py
@@ -1,4 +1,8 @@
-log_file = "/var/log/auth.log"
+import re
+
+pattern = r"\d+\.\d+\.\d+\.\d+"
+
+log_file = r"C:\Users\mdsid\Documents\siddik-security\SOC\auth.log"
 
 count = 0
 
@@ -6,6 +10,10 @@ with open(log_file) as f:
         for line in f:
                 if "session opened for" in line or "Failed password" in line:
                         count += 1
-                        print(line.strip())
+                        #print(line.strip())
+                        match = re.search(pattern, line)
+                        ip = match.group() if match else "No IP found"
+                        timestamp = " ".join(line.split()[0:3])
+                        print(f"{timestamp} -> Failed password from {ip}")
 
 print("\nTotal Failed Logins:", count)
